b2cb3b5b0245fad98a3e86b43238292b85c3693f
minz1 <emerytang@gmail.com>
2017-11-11 04:56:50 -0600

    build_script: repopicks: update repopick list

--- repopicks.sh
+++ repopicks.sh
@@ -1,36 +1,55 @@
-#!/bin/bash
-
 . build/envsetup.sh
-echo -e ${cya}"Picking changes from Lineage gerrit..." ${txtrst};
-# Repopicks begin here
 
-git config --global user.email "emerytang@gmail.com"
-git config --global user.name "minz1"
-
-# frameworks/av -> libstagefright: add NV21 color #
-repopick 187892
-
-# ssystem/core -> init: don't reboot to bootloader on panic #
+# system/core -> init: don't reboot to bootloader on panic #
 repopick 187146
+
+# ssystem/core -> Restore support for legacy f_adb interface #
+repopick 185642
+repopick 186246
 
 # vendor/lineage -> apns-conf: Update Ukrainian APNs #
 repopick 187718
 
-# shims
+# hardware/samsung
+repopick 185890
+repopick 190989
+repopick 190990
+repopick 190991
+repopick 190992
+repopick 190995
+repopick 191028
+repopick 191029
+repopick 191030
+repopick 195868
+
+# ril
+repopick 195755
+
+# android-o-camera-hal + sepolicy #
+repopick 186244
+repopick 186245
+
+# qcom-common
+repopick 191347
+
+# android-o-camera-hal #   frameworks_av
+repopick 187558 # libcameraservice: Don't pass NULL args on setCallbacks call
+repopick 187559 # camera/media: Support for legacy camera HALv1   Y
+repopick 187560 # libstagefright: Support for legacy camera/encoder buffers?
+repopick 187561 # OMXNodeInstance: Fix legacy HAL1 after merge of android-7.1.0_r7
+repopick 186713 -f #camera parameters extra support
+
+# frameworks/av -> libstagefright: add NV21 color #
+#repopick 187892
+#!!!!https://review.lineageos.org/#/c/194415/
+
+# shims android_bionic
 repopick 185640
 
-# media codecs
+#media codecs frameworks_base
 repopick 189539
 
-# samsung/qcom-common
-repopick 191347; # [DNM] qcom-common: O bringup
-
-# qcom dataservices
+# yes
 cd vendor/qcom/opensource/dataservices
 git fetch https://review.lineageos.org/LineageOS/android_vendor_qcom_opensource_dataservices refs/changes/64/181464/1 && git cherry-pick FETCH_HEAD
 cd ../../../..
-
-### hardware/samsung
-repopick 190634; # Add references to liblog
-repopick 195655; # macloader: Move device dependent modules to /vendor
-repopick -Q 'status:open+topic:samsung-libril-oreo'; ## _<sup><sub>https://review.lineageos.org/#/q/status:open+topic:samsung-libril-oreo</sup></sub>
